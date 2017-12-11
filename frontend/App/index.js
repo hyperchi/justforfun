21056497fccd5d2211a55c9db602354527992087
hyperchi <hyperbolechi@gmail.com>
2017-12-10 23:04:05 -0500

    remove sth

--- frontend/App/index.js
+++ frontend/App/index.js
@@ -19,20 +19,20 @@ import UserProfile from '../Views/UserProfile';
 import NotFound from '../Views/NotFound';
 
 ReactDOM.render (
-  <Provider store={appStore}>
-    <Router history={browserHistory}>
-      <Route path="/admin" component={AdminContainer}>
-        <IndexRoute component={Dashboard} />
-      </Route>
-      <Route path="/" component={AppContainer}>
-        <IndexRoute component={ForumFeed} />
-        <Route path=":forum" component={ForumFeed} />
-        <Route path=":forum/discussion/:discussion" component={SingleDiscussion} />
-        <Route path=":forum/new_discussion" component={NewDiscussion} />
-        <Route path="user/:username" component={UserProfile} />
-        <Route path="*" component={NotFound} />
-      </Route>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={appStore}>
+      <Router history={browserHistory}>
+        <Route path="/admin" component={AdminContainer}>
+          <IndexRoute component={Dashboard} />
+        </Route>
+        <Route path="/" component={AppContainer}>
+          <IndexRoute component={ForumFeed} />
+          <Route path=":forum" component={ForumFeed} />
+          <Route path=":forum/discussion/:discussion" component={SingleDiscussion} />
+          <Route path=":forum/new_discussion" component={NewDiscussion} />
+          <Route path="user/:username" component={UserProfile} />
+          <Route path="*" component={NotFound} />
+        </Route>
+      </Router>
+    </Provider>,
+    document.getElementById('root')
 );
